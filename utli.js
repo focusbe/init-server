ef292b3040a9f610dc9436089dc3dddd7f11b395
pengzai <roc.liu@hotmail.com>
2021-03-21 22:26:48 +0800

    同步

--- utli.js
+++ utli.js
@@ -7,13 +7,13 @@ function exec(execStr) {
     const execArr = execStr.replace(/\s+/g, ' ').split(' ');
     const command = execArr.shift();
     const ls = spawn(command, execArr, {});
-    ls.stdout.on('data', (data) => {
-      console.log(`${data}`);
-    });
-    ls.stderr.on('data', (data) => {
-      console.error(`${data}`);
-      reject(data);
-    });
+    // ls.stdout.on('data', (data) => {
+    //   console.log(`${data}`);
+    // });
+    // ls.stderr.on('data', (data) => {
+    //   console.error(`${data}`);
+    //   reject(data);
+    // });
     ls.on('close', (code) => {
       resolve(code);
     });
